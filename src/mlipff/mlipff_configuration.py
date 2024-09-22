53bb86429679ec577f0bf78f2ac07f2ed9cb3d37
IlyaRaven <feniksaven@gmail.com>
2024-09-22 17:49:04 +0300

    Initial commit for MLIPFF project

---
+++ src/mlipff/mlipff_configuration.py
@@ -0,0 +1,298 @@
+class Configuration:
+    """
+    A class to represent a single configuration of atoms.
+
+    Attributes
+    ----------
+    size : int
+        The number of atoms in the configuration.
+    atom_data : list of dict
+        The list of atom data with properties id, type, x, y, z, fx, fy, fz.
+    supercell : list of float, optional
+        The supercell matrix describing the configuration.
+    energy : float, optional
+        The energy of the configuration.
+    plus_stress : dict, optional
+        Stress information of the configuration.
+    features : dict, optional
+        Additional features of the configuration.
+    """
+
+    def __init__(
+        self,
+        size=None,
+        atom_data=None,
+        supercell=None,
+        energy=None,
+        plus_stress=None,
+        features=None,
+    ):
+        self.size = size
+        self.atom_data = atom_data
+        self.supercell = supercell or []
+        self.energy = energy or 0
+        self.plus_stress = plus_stress or {}
+        self.features = features or {}
+
+    def read_cfg(self, file_path):
+        """Reads a single configuration from a .cfg file."""
+        with open(file_path, "r") as file:
+            atom_data = []
+            reading_atoms = False
+            file_iter = iter(file)
+
+            for line in file_iter:
+                line = line.strip()
+
+                if "BEGIN_CFG" in line:
+                    continue
+
+                if "Size" in line:
+                    self.size = int(next(file).strip())
+                    continue
+                elif "Supercell" in line:
+                    self.supercell = []
+                    for _ in range(3):
+                        line = next(file).strip()
+                        if "AtomData:" in line:
+                            reading_atoms = True
+                            atom_headers = line.split()[1:]
+                            break
+                        else:
+                            self.supercell.extend(map(float, line.split()))
+                    continue
+                elif "AtomData:" in line:
+                    reading_atoms = True
+                    atom_headers = line.split()[1:]
+                    continue
+                elif reading_atoms:
+                    if line == "":
+                        reading_atoms = False
+                    else:
+                        atom_values = line.split()
+                        atom = {}
+                        for i, value in enumerate(atom_values):
+                            if atom_headers[i] in ["id", "type"]:
+                                atom[atom_headers[i]] = int(value)
+                            else:
+                                atom[atom_headers[i]] = float(value)
+                        atom_data.append(atom)
+                    if len(atom_data) == self.size:
+                        reading_atoms = False
+                        self.atom_data = atom_data
+                        continue
+                elif "Energy" in line:
+                    self.energy = float(next(file).strip())
+                elif "PlusStress" in line:
+                    stress_headers = line.split()[1:]
+                    stress_values = list(map(float, next(file).strip().split()))
+                    self.plus_stress = dict(zip(stress_headers, stress_values))
+                elif "Feature" in line:
+                    feature_parts = line.split(maxsplit=2)
+                    if len(feature_parts) == 3:
+                        feature_name = feature_parts[1]
+                        feature_value = feature_parts[2]
+                        self.features[feature_name] = feature_value
+
+    def write_cfg(self, file_path):
+        """Writes the configuration to a .cfg file."""
+        with open(file_path, "w") as file:
+            file.write("BEGIN_CFG\n")
+            file.write(f"Size\n{self.size}\n")
+            if self.supercell:
+                file.write("Supercell\n")
+                for i in range(0, len(self.supercell), 3):
+                    file.write(f"{' '.join(map(str, self.supercell[i:i+3]))}\n")
+            if self.atom_data:
+                headers = " ".join(self.atom_data[0].keys())
+                file.write(f"AtomData: {headers}\n")
+                for atom in self.atom_data:
+                    file.write(" ".join(map(str, atom.values())) + "\n")
+            if self.energy is not None:
+                file.write(f"Energy\n{self.energy}\n")
+            if self.plus_stress:
+                file.write("PlusStress: " + " ".join(self.plus_stress.keys()) + "\n")
+                file.write(" ".join(map(str, self.plus_stress.values())) + "\n")
+            if self.features:
+                for name, value in self.features.items():
+                    file.write(f"Feature {name} {value}\n")
+            file.write("END_CFG\n\n")
+
+    def read_dump(self, file_path, include_efs=False):
+        """
+        Reads a LAMMPS .dump file and populates the configuration attributes.
+
+        Parameters
+        ----------
+        file_path : str
+            The path to the .dump file to read from.
+        include_efs : bool
+            Whether to include energy, forces, and stress data.
+        """
+        with open(file_path, "r") as lmp_dump:
+            lines = lmp_dump.readlines()
+
+        lattice = [0.0, 0.0, 0.0]
+        data = []
+        in_atoms_section = False
+        in_box_section = False
+
+        for line in lines:
+            if "ITEM: NUMBER" in line:
+                self.size = int(lines[lines.index(line) + 1].strip())
+            elif "ITEM: BOX" in line:
+                in_box_section = True
+            elif in_box_section:
+                lattice[lines.index(line) % 3] = float(line.split()[1])
+                if lines.index(line) % 3 == 2:
+                    in_box_section = False
+                    self.supercell = [
+                        lattice[0],
+                        0,
+                        0,
+                        0,
+                        lattice[1],
+                        0,
+                        0,
+                        0,
+                        lattice[2],
+                    ]
+            elif "ITEM: ATOMS" in line:
+                in_atoms_section = True
+                coordinates_start_index = line.split().index("x") - 2
+                if "fx" in line:
+                    include_efs = True
+                    forces_start_index = line.split().index("fx") - 2
+                else:
+                    include_efs = False
+            elif in_atoms_section:
+                atoms_data = list(map(float, line.split()))
+                atom_type = int(atoms_data[1] - 1)
+                pos_x, pos_y, pos_z = atoms_data[
+                    coordinates_start_index : coordinates_start_index + 3
+                ]
+                f_x, f_y, f_z = (
+                    atoms_data[forces_start_index : forces_start_index + 3]
+                    if include_efs
+                    else (0, 0, 0)
+                )
+                self.energy += (
+                    atoms_data[forces_start_index + 3] if include_efs else None
+                )
+                atom = {
+                    "id": len(data) + 1,
+                    "type": atom_type,
+                    "cartes_x": pos_x,
+                    "cartes_y": pos_y,
+                    "cartes_z": pos_z,
+                    "fx": f_x,
+                    "fy": f_y,
+                    "fz": f_z,
+                }
+                data.append(atom)
+
+        self.atom_data = data
+
+    def read_orca_dump(self, filename):
+        """
+        Reads ORCA .log and LAMMPS .dump files, creating substracted configuration
+        QM - MM
+
+        Parameters
+        ----------
+        filename : str
+            The base filename (without extension) of the .log and .dump files.
+        """
+
+        if len(filename.split(".")) != 1:
+            filename = filename.split(".")[0]
+        logname = f"{filename}.log"
+        dumpname = f"{filename}.dump"
+
+        # Constants
+        Eh = 27.2113834
+        Bohr = 0.5291772083
+
+        # Read initial configuration from .dump file
+        mm_config = Configuration()
+        mm_config.read_dump(dumpname, include_efs=True)
+
+        # Create a new configuration with the same atomic positions but different forces/energy
+        qm_config = Configuration(
+            size=mm_config.size,
+            atom_data=[
+                {
+                    "id": atom["id"],
+                    "type": atom["type"],
+                    "cartes_x": atom["cartes_x"],
+                    "cartes_y": atom["cartes_y"],
+                    "cartes_z": atom["cartes_z"],
+                    "fx": atom["fx"],
+                    "fy": atom["fy"],
+                    "fz": atom["fz"],
+                }
+                for atom in mm_config.atom_data
+            ],
+            supercell=mm_config.supercell,
+            energy=mm_config.energy,
+        )
+
+        # Read forces and energy from .log file
+        with open(logname, "r") as logfile:
+            lines = logfile.readlines()
+
+        try:
+            energy_line = next(
+                line for line in lines if "FINAL SINGLE POINT ENERGY" in line
+            )
+            qm_energy = float(energy_line.split()[4]) * Eh - mm_config.energy
+        except StopIteration:
+            raise ValueError("Energy not found in ORCA log file")
+
+        qm_config.energy = qm_energy
+
+        try:
+            grad_start = lines.index("CARTESIAN GRADIENT\n") + 3
+            for atom, line in zip(qm_config.atom_data, lines[grad_start:]):
+                force_data = list(map(float, line.split()[3:6]))
+                atom["fx"] = -force_data[0] * Eh / Bohr - atom["fx"]
+                atom["fy"] = -force_data[1] * Eh / Bohr - atom["fy"]
+                atom["fz"] = -force_data[2] * Eh / Bohr - atom["fz"]
+        except (ValueError, IndexError):
+            raise ValueError("Gradient data not found or incomplete in ORCA log file")
+
+        # Replace the original configuration's atom data and energy with updated values
+        self.size = qm_config.size
+        self.atom_data = qm_config.atom_data
+        self.supercell = []
+        self.energy = qm_config.energy
+
+    def __sub__(self, other):
+        """Subtracts the fields fx, fy, fz, Energy, and PlusStress from another Configuration object."""
+        new_atom_data = []
+        for atom1, atom2 in zip(self.atom_data, other.atom_data):
+            new_atom = {}
+            for key in atom1:
+                if key in ["fx", "fy", "fz"]:
+                    new_atom[key] = atom1.get(key, 0.0) - atom2.get(key, 0.0)
+                else:
+                    new_atom[key] = atom1[key]
+            new_atom_data.append(new_atom)
+
+        new_plus_stress = {
+            key: self.plus_stress.get(key, 0.0) - other.plus_stress.get(key, 0.0)
+            for key in self.plus_stress
+        }
+
+        return Configuration(
+            size=self.size,
+            atom_data=new_atom_data,
+            supercell=self.supercell,
+            energy=(
+                (self.energy - other.energy)
+                if self.energy is not None and other.energy is not None
+                else None
+            ),
+            plus_stress=new_plus_stress,
+            features=self.features,
+        )
---
+++ src/mlipff/utils.py
@@ -0,0 +1,212 @@
+import random
+import os
+import shutil
+from mlipff_configuration import Configuration
+from ocellar import molecule
+
+
+def convert(from_format, to_format, input_file, output_file):
+    config = Configuration()
+
+    if from_format == "dump":
+        config.read_dump(input_file)
+    elif from_format == "orca":
+        config.read_orca_dump(input_file)
+    else:
+        print(f"Unsupported input format: {from_format}")
+        return
+
+    if to_format == "cfg":
+        config.write_cfg(output_file)
+    else:
+        print(f"Unsupported output format: {to_format}")
+
+
+def cut_data(data_file, dump_file, output_file):
+    tmp_num = random.randint(100000, 999999)
+    tmp_dir = f"data_tmp_{tmp_num}"
+    os.mkdir(tmp_dir)
+    tmp1_path = os.path.join(tmp_dir, "tmp1.txt")
+    tmp2_path = os.path.join(tmp_dir, "tmp2.txt")
+    tmp3_path = os.path.join(tmp_dir, "tmp3.txt")
+
+    with open(data_file, "r") as datafile, open(dump_file, "r") as dumpfile, open(
+        tmp1_path, "w"
+    ) as tmp1, open(tmp2_path, "w") as tmp2, open(tmp3_path, "w") as tmp3, open(
+        output_file, "w"
+    ) as output:
+        ids = set()
+        read_number = False
+        read_atoms = False
+
+        for line in dumpfile:
+            if "ITEM: NUMBER" in line:
+                read_number = True
+                continue
+            if read_number:
+                atom_number = int(line.strip())
+                read_number = False
+            if "ITEM: ATOMS" in line:
+                read_atoms = True
+                continue
+            if read_atoms:
+                ids.add(int(line.split()[0]))
+                if len(ids) == atom_number:
+                    break
+
+        parsarg = ""
+        line_counter = 0
+        atoms = bonds = angles = dihedrals = impropers = 0
+
+        for line in datafile:
+            if line_counter <= 23:
+                tmp1.write(line)
+                line_counter += 1
+                continue
+            if line == "\n":
+                tmp2.write(line)
+                continue
+            if "Atoms" in line:
+                parsarg = "atoms"
+                tmp2.write(line)
+                continue
+            if "Bonds" in line:
+                parsarg = "bonds"
+                tmp2.write(line)
+                continue
+            if "Angles" in line:
+                parsarg = "angles"
+                tmp2.write(line)
+                continue
+            if "Dihedrals" in line:
+                parsarg = "dihedrals"
+                tmp2.write(line)
+                continue
+            if "Impropers" in line:
+                parsarg = "impropers"
+                tmp2.write(line)
+                continue
+            if parsarg == "atoms":
+                if int(line.split()[0]) in ids:
+                    atoms += 1
+                    tmp2.write(line)
+            if parsarg == "bonds":
+                if int(line.split()[2]) in ids or int(line.split()[3]) in ids:
+                    bonds += 1
+                    tmp2.write(line)
+            if parsarg == "angles":
+                if (
+                    int(line.split()[2]) in ids
+                    or int(line.split()[3]) in ids
+                    or int(line.split()[4]) in ids
+                ):
+                    angles += 1
+                    tmp2.write(line)
+            if parsarg == "dihedrals":
+                if (
+                    int(line.split()[2]) in ids
+                    or int(line.split()[3]) in ids
+                    or int(line.split()[4]) in ids
+                    or int(line.split()[5]) in ids
+                ):
+                    dihedrals += 1
+                    tmp2.write(line)
+            if parsarg == "impropers":
+                if (
+                    int(line.split()[2]) in ids
+                    or int(line.split()[3]) in ids
+                    or int(line.split()[4]) in ids
+                    or int(line.split()[5]) in ids
+                ):
+                    impropers += 1
+                    tmp2.write(line)
+
+        with open(tmp1_path, "r") as tmp1:
+            for line in tmp1:
+                if "atoms" in line:
+                    tmp3.write(f"     {atoms}  atoms\n")
+                elif "bonds" in line:
+                    tmp3.write(f"     {bonds}  bonds\n")
+                elif "angles" in line:
+                    tmp3.write(f"     {angles}  angles\n")
+                elif "dihedrals" in line:
+                    tmp3.write(f"     {dihedrals}  dihedrals\n")
+                elif "impropers" in line:
+                    tmp3.write(f"     {impropers}  impropers\n")
+                else:
+                    tmp3.write(line)
+
+        shutil.copyfileobj(open(tmp3_path, "r"), output)
+        shutil.copyfileobj(open(tmp2_path, "r"), output)
+
+    shutil.rmtree(tmp_dir)
+
+
+def generate_orca_input(xyz_filename):
+
+    input_lines = """\
+    !ENGRAD PBE0 def2-SVP
+    *xyzfile 0 1 {xyz_filename}
+    
+    """
+
+    # Check if the file exists and has the correct extension
+    if not xyz_filename.endswith(".xyz") or not os.path.isfile(xyz_filename):
+        print("Error: Please provide a valid .xyz file.")
+        return
+
+    inp_filename = os.path.splitext(xyz_filename)[0] + ".inp"
+
+    try:
+        with open(inp_filename, "w") as inpfile:
+            inpfile.write(input_lines.format(xyz_filename=xyz_filename))
+    except IOError as e:
+        print(f"Error writing to file {inp_filename}: {e}")
+
+
+def cut_ff(input_filename="system.in.settings", style="lj/cut/coul/long"):
+    # Get the directory of the input file
+    input_dir = os.path.dirname(os.path.abspath(input_filename))
+
+    # Define the output file paths in the same directory as the input file
+    pair_file_path = os.path.join(input_dir, "pair_coefs.ff")
+    other_file_path = os.path.join(input_dir, "other_coefs.ff")
+
+    # Open the input file and two output files
+    with open(input_filename, "r") as ff_file, open(
+        pair_file_path, "w"
+    ) as pair_file, open(other_file_path, "w") as other_file:
+
+        # Process the input file
+        for line in ff_file:
+            if "pair_coeff" in line:
+                parsed = line.split(" ")
+                start, end = parsed[:3], parsed[3:]
+                pair_file.write(f"{' '.join(start)} {style} {' '.join(end)}")
+            else:
+                other_file.write(line)
+
+
+def cut_nbh(input_name, out_base, atom_coords, radius, cut):
+    out_dump = str(out_base) + ".dump"
+    out_xyz = str(out_base) + ".xyz"
+    # from dump
+    mol = molecule.Molecule()
+    mol.input_geometry = input_name
+    mol.build_geometry(
+        backend="MDAnalysis"
+    )  # Build geometry from input file with MDAnalysis backend
+    mol.build_graph()
+    mol.build_structure(cut_molecule=cut)
+
+    new_mol, idxs = mol.select(
+        mol.select_r(atom_coords, radius)
+    )  # Select atom idxs with sphere center and radius, then build a new Molecule and idxs
+
+    mol.save_dump(
+        out_dump, mol.input_geometry, idxs
+    )  # Save dump file using original dump and idxs
+    
+    new_mol.build_graph()
+    new_mol.build_structure(cut_molecule=cut)
+    new_mol.save_xyz(out_xyz)
